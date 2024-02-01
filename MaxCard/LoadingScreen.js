bff4feabba6786efbe4ad517a045c7968d7c8e62
OGuess10 <66238560+OGuess10@users.noreply.github.com>
2024-02-01 17:17:21 -0600

    Seperated LoadingScreen from App.js and added spinning animation

---
+++ MaxCard/LoadingScreen.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Dimensions, Animated, Easing } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import {
+  useFonts,
+  Jost_500Medium,
+} from '@expo-google-fonts/jost';
+
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
+
+const Spin = () => {
+  const [spinValue, setSpinValue] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.loop(
+    Animated.timing(spinValue, {
+      toValue: 1,
+      duration: 2000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    })).start();
+  }, []);
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  return (
+    <Animated.View style={{transform: [{rotate: spin}]}}>
+    <LinearGradient
+        colors={['#63989D', 'rgba(214, 243, 213, 0)']}
+        style={styles.circle2} />
+    </Animated.View>
+  );
+};
+
+export function LoadingScreen() {
+  let [fontsLoaded] = useFonts({Jost_500Medium,});
+  if(!fontsLoaded) {
+    return;
+  } else {
+    return (
+      <View style={styles.container}>
+        <LinearGradient
+          colors={['#2C506F', 'black']}
+          style={styles.background}>
+          <LinearGradient
+            colors={['#D6F3D5', 'rgba(214, 243, 213, 0)']}
+            style={styles.circle} />
+          <Spin />
+          <Text style={styles.text}>Loading...</Text>
+        </LinearGradient>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  background: {
+    height: screenHeight,
+    width: screenWidth,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'Jost_500Medium',
+    fontSize: 24,
+    letterSpacing: 4,
+    color: 'white',
+    padding: 15,
+  },
+
+  circle: {
+    width: 283,
+    height: 283,
+    borderRadius: 283/2,
+    position: 'absolute',
+    left: screenWidth / 2 - 283 / 2,
+    top: (screenHeight / 2 - 283 / 2) - 32,
+  },
+  circle2: {
+    width: 283,
+    height: 283,
+    borderRadius: 283/2,
+  }
+
+});
