907e9ba0ded61201b2e61d03ed1b7f84dcf0774e
OGuess10 <66238560+OGuess10@users.noreply.github.com>
2024-02-06 16:56:01 -0600

    Added data javascript file as interface between server and frontend

--- MaxCard/App.js
+++ MaxCard/App.js
@@ -3,5 +3,6 @@ import { LoadingScreen } from './views/LoadingScreen';
 export default function App() {
   return (
     <LoadingScreen />
+    // <LocationScreen/ > which returns a view after getlocationcards returns
   )
 }
