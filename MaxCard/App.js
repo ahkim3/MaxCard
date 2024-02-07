38c7cd4dab75a2479bd74d21eecc99a24714f353
tylerharsell <tharsell20@gmail.com>
2024-02-07 13:27:52 -0600

    home page design and gitignore additions

--- MaxCard/App.js
+++ MaxCard/App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View} from "react-native";
+import Home  from './Home';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
