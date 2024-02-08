69898c01021498f4d443af9520d7b275892b5424
Eliboll <eetes77@gmail.com>
2024-02-08 16:35:24 -0600

    added verbose output

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -8,8 +8,14 @@ echo "--Installing Requirements--"
 python3 -m pip install -r requirements.txt
 
 echo "--starting server--"
-nohup python3 ./app.py > output.log 2>&1 &
+python3 ./app.py > output.log 2>&1 &
 
-sleep 10
-echo "--server running server--"
+if [ $? -eq 0 ]
+then
+  echo "--server running server--"
+else
+  echo "Error encountered while starting the server. Check output.log for details."
+  exit 1
+fi
+
 exit 0
