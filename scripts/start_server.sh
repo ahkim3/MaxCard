a44a5234f70e84912aa5a48719c0db7584489437
Eliboll <eetes77@gmail.com>
2024-02-08 17:53:54 -0600

    i think it works!

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -14,6 +14,7 @@ screen -d -m python3 ./app.py
 if [ $? -eq 0 ]
 then
   echo "--server running server--"
+  exit 0
 else
   echo "Error encountered while starting the server. Check output.log for details."
   exit 1
