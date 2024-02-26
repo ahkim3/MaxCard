7d0617b227c0fc43b726c997b178c42f1812dab1
OGuess10 <66238560+OGuess10@users.noreply.github.com>
2024-02-26 17:38:28 -0600

    Rounded buttons!

--- MaxCard/views/AlternateLocations.js
+++ MaxCard/views/AlternateLocations.js
@@ -12,13 +12,13 @@ const screenWidth = Dimensions.get('window').width;
 
 const Button = props => {
   return(
-    <LinearGradient
-      colors={['#205072', '#51999E']}
-      styles={styles.buttonContainer}>
-      <TouchableOpacity style={styles.button} onPress={console.log("Button pressed")}>
-        <Text style={styles.text}>{props.location}</Text>
-      </TouchableOpacity>
-    </LinearGradient>
+    <TouchableOpacity style={styles.buttonContainer} onPress={console.log("Button pressed")}>
+      <LinearGradient
+        colors={['#205072', '#51999E']}
+        style={styles.button}>
+          <Text style={styles.text}>{props.location}</Text>
+      </LinearGradient>
+    </TouchableOpacity>
   );
 }
 
@@ -42,15 +42,13 @@ export function AlternateLocations() {
         <Button location='location 3'/>
         <Button location='location 4'/>
         <Button location='location 5'/>
-        <View styles={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttonContainer} onPress={console.log("Button pressed")}>
           <LinearGradient
             colors={['#51999E', '#7BE495']}
-            styles={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button} onPress={console.log("Button pressed")}>
+            style={styles.button}>
               <Text style={styles.text}>Retry</Text>
-            </TouchableOpacity>
           </LinearGradient>
-        </View>
+        </TouchableOpacity>
         <View style={{position: 'absolute', top: screenHeight}}>
           <NavBar/>
         </View>
@@ -77,9 +75,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     letterSpacing: 4,
     color: 'white',
-    borderRadius: 30,
-    borderWidth: 5,
-    borderColor: 'cyan'
+    borderRadius: 30
   },
   title: {
     fontFamily: 'Jost_700Bold',
@@ -91,53 +87,14 @@ const styles = StyleSheet.create({
   buttonContainer: {
     height: 53,
     width: 196,
-    borderRadius: 100,
-    overflow: 'hidden',
-    padding: 30,
-    borderWidth: 5,
-    borderColor: 'blue'
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    borderRadius: 30
   },
   button: {
-    height: 53,
-    width: 196,
-    borderRadius: 30,
-    overflow: 'hidden',
-    justifyContent: 'center',
+    borderRadius:30,
     alignItems: 'center',
-    borderWidth: 5,
-    borderColor: 'red'
-  },
-  container: {
-    height: 100,
-    width: 100,
-    backgroundColor: 'red',
-  },
-  navContainer: {
-    position: "relative",
-  },
-  nav: {
-    position: "absolute",
-    right: -0.5 * screenWidth,
-    top: -0.2 * screenHeight,
-    flexDirection: "row",
-    width: screenWidth,
-    justifyContent: "space-evenly",
-  },
-  navItems: {
-    marginHorizontal: 10,
-  },
-  navWallet: {
-    height: 0.2 * screenHeight,
-    width: 0.2 * screenWidth,
-  },
-  navSettings: {
-    marginTop: 0.02 * screenHeight,
-    height: 0.15 * screenHeight,
-    width: 0.15 * screenWidth,
-  },
-  navBack: {
-    fontSize: 90,
-    color: "gray",
-    marginTop: 0.03 * screenHeight,
-  },
+    height: '100%',
+    justifyContent: 'center'
+  }
 });
